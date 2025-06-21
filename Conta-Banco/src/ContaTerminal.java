311d300576d6bfdfbf0181a78eeee727afc24c7d
zemilson <zemilson@Z.com>
2025-06-21 00:02:02 -0300

    atualizando2

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,6 +1,6 @@
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("ROSELENE");
     }
 }
