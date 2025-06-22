88e9024d4ae12a3248f3139a0f0cc4b72bdf522b
zemilson <zemilson@Z.com>
2025-06-22 00:20:30 -0300

    cod1

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("ROSELENE");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System .in);
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+        System.out.println("por favor, digite o número da agência:");
+        String numeroAgencia = scanner.nextLine();
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldoConta = scanner.nextDouble();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua conta é " + numeroConta + " e sua agência é " + numeroAgencia + ".");
+        System.out.printf("Seu saldo é de R$ %.2f%n", saldoConta);
+        scanner.close();
     }
 }
